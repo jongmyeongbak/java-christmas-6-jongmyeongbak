8ee8f0c227315c572920c0049844dc4059b0b992
jongmyeongbak <withpleasure@gmail.com>
2023-11-14 15:37:01 +0900

    refactor(DateValidatorTest): AssertJ를 적용하여 일관성 향상

--- src/test/java/christmas/util/DateValidatorTest.java
+++ src/test/java/christmas/util/DateValidatorTest.java
@@ -1,7 +1,7 @@
 package christmas.util;
 
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -11,7 +11,8 @@ class DateValidatorTest {
     @ParameterizedTest
     @ValueSource(ints = {1, 31})
     void validDateDoesNotThrowException(int date) {
-        assertDoesNotThrow(() -> DateValidator.validateDecemberDateRange(date));
+        assertThatCode(() -> DateValidator.validateDecemberDateRange(date))
+                .doesNotThrowAnyException();
     }
 
     @ParameterizedTest
@@ -19,6 +20,6 @@ class DateValidatorTest {
     void invalidDateThrowsException(int date) {
         assertThatThrownBy(() -> DateValidator.validateDecemberDateRange(date))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+                .hasMessageContaining("유효하지 않은 날짜입니다.");
     }
 }
